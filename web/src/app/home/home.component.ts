cd4d825414bce3b2be406e4dd12196858a936b33
deb <debkbanerji@gmail.com>
2017-11-17 13:49:44 -0500

    Add CIrcularness

--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -7,14 +7,24 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+    // Array List
     public arrayListSorted = false;
     public arrayListSizeStored = true;
     public kthSelectionAlgorithms = [
         'Random',
         'Median of Medians'
     ];
-
     public kthSelectionAlgorithm = this.kthSelectionAlgorithms[0];
+
+    // Linked List
+    public linkedListHasTailPointer = true;
+    public linkedListSizeStored = true;
+    public linkedListCircular = false;
+    public linkedListTypes = [
+        'Singly Linked',
+        'Doubly Linked'
+    ];
+    public linkedListType = this.linkedListTypes[0];
 
     constructor() {
     }
