bac0dc4bc6819be2e691b48fcf91a534ad57b6e2
deb <debkbanerji@gmail.com>
2017-11-16 20:19:13 -0500

    Bootstrap Web Application

---
+++ web/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+
+import {RouterModule, Routes} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatCheckboxModule} from '@angular/material';
+
+
+
+const routes: Routes = [ // Array of all routes - modify when adding routes
+    {path: '', component: HomeComponent} // Default route
+];
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatCheckboxModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
