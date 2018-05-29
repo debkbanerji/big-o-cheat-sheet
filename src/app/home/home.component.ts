a173221e1bacb275fb67ac36dee4fab4fee69d07
deb <debkbanerji@gmail.com>
2018-05-28 18:23:29 -0700

    Update Version Displaying

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {environment} from '../../environments/environment';
 
 @Component({
     selector: 'app-home',
@@ -6,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+    public version: string = environment.VERSION;
 
     public commonSetOperations = [
         'Accessing an element',
